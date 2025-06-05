d5532615c19297906366b01f3308fe8aa6d5d418
aryanandoria <122526574+aryanandoria@users.noreply.github.com>
2025-06-05 10:30:12 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
-// In-memory store
+
 const store = {
-  ingestionRequests: {}, // { ingestion_id: { status, batches: [] } }
-  queue: [] // [{ priorityValue, timestamp, batch }]
+  ingestionRequests: {}, 
+  queue: [] 
 };
 
 module.exports = store;
